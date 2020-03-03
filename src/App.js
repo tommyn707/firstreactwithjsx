2d30ce7a8e12f1b0ba362d8c3095470c566a949c
tommyn707 <tommy.n707@gmail.com>
2020-03-02 16:39:57 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyNewComponents from './components/MyNewComponents.js';
+
+function App() {
+  return (
+    <div className="App">
+        <MyNewComponents />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyNewComponents.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class MyNewComponents extends Component {
+    render() {
+        return (<div>
+    <h1>Hello World</h1>
+    </div>)
+    }
+}
+export default MyNewComponents;
